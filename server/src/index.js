bfa4913924e2830be97c1f4a86b94e58c5c886f1
Kashif <ahmed.kashif1001@gmail.com>
2025-07-28 22:44:53 +0500

    file and folder structure and server setup

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import "dotenv/config";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+
+import { connectDB } from "./lib/db.js"
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+//mounts the authRoutes
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
